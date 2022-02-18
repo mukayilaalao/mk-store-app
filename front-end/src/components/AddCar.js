0759376df98f94043ae5b9447ccb48bd7c4aeaab
Mukayila ALAO <mukayilaalao@pursuit.org>
2022-02-18 07:59:02 -0500

    created a form component

--- front-end/src/components/AddCar.js
+++ front-end/src/components/AddCar.js
@@ -1,21 +1,5 @@
 import { useState, useEffect } from "react";
 const API = process.env.REACT_APP_API_URL;
-function AddCar() {
-  const [car, setCar] = useState({
-    vim: "",
-    name: "",
-    description: "",
-    color: "",
-    image: "",
-    price: null,
-    rating: 5,
-    featured: false,
-  });
-  return (
-    <div>
-      <form></form>
-    </div>
-  );
-}
+function AddCar() {}
 
 export default AddCar;
---
+++ front-end/src/components/Form.js
@@ -0,0 +1,106 @@
+function Form() {
+  const [car, setCar] = useState({
+    vim: "",
+    name: "",
+    description: "",
+    color: "",
+    image: "",
+    price: null,
+    rating: 5,
+    featured: false,
+  });
+  const handleTextChange = (e) => {
+    if (e.target.id === "featured")
+      setCar({ ...car, featured: e.target.checked });
+    else setCar({ ...car, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="vim">Vim</label>
+        <br />
+        <input
+          type="text"
+          id="vim"
+          value={car.vim}
+          onChange={handleTextChange}
+          required
+        />
+        <hr />
+        <label htmlFor="name">Name</label>
+        <br />
+        <input
+          type="text"
+          id="name"
+          value={car.name}
+          onChange={handleTextChange}
+          required
+        />
+        <hr />
+        <label htmlFor="description">Description</label>
+        <br />
+        <textarea
+          id="description"
+          onChange={handleTextChange}
+          placeholder="Car description..."
+        >
+          {car.description}
+        </textarea>
+        <hr />
+        <label htmlFor="color">Color</label>
+        <br />
+        <input
+          type="text"
+          id="color"
+          value={car.color}
+          onChange={handleTextChange}
+        />
+        <hr />
+        <label htmlFor="image">Image</label>
+        <br />
+        <input
+          type="text"
+          id="image"
+          value={car.image}
+          onChange={handleTextChange}
+          placeholder="https://"
+        />
+        <hr />
+        <label htmlFor="price">Price</label>
+        <br />
+        <input
+          type="number"
+          id="price"
+          value={car.price}
+          onChange={handleTextChange}
+          required
+        />
+        <hr />
+        <label htmlFor="rating">Rating</label>
+        <br />
+        <input
+          type="number"
+          id="rating"
+          value={car.rating}
+          onChange={handleTextChange}
+        />
+        <hr />
+        <label htmlFor="featured">Featured</label>
+        <br />
+        <input
+          type="checkbox"
+          id="featured"
+          checked={checked}
+          onChange={handleTextChange}
+        />
+        <hr />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
