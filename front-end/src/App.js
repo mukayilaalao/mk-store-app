ced5c483817719a16ae011aec9368d994ccc582b
Mukayila ALAO <mukayilaalao@pursuit.org>
2022-02-17 20:41:40 -0500

    Added all routes in App

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,30 +1,20 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const API = process.env.REACT_APP_API_URL;
-
-console.log(API);
-function App() {
-  const [days, setDays] = useState([]);
-  useEffect(() => {
-    axios
-      .get(`${API}/test`)
-      .then(
-        (response) => {
-          setDays(response.data);
-        },
-        (error) => console.log("get", error)
-      )
-      .catch((c) => console.warn("catch", c));
-  }, []);
+const App = () => {
   return (
-    <div>
-      <ul>
-        {days.map((day) => (
-          <li key={day.name}>{day.name}</li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<Index />} />
+        <Route path="/cars/new" element={<New />} />
+        <Route path="/cars/:id" element={<Show />} />
+        <Route path="/cars/:id/edit" element={<Edit />} />
+        <Route path="*" element={<FourOFour />} />
+      </Routes>
+      <Foot />
+    </Router>
   );
-}
+};
 
 export default App;
