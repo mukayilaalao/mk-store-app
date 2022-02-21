80c30efa409a5ee9a711365089094e9f65ed68ed
Mukayila ALAO <mukayilaalao@pursuit.org>
2022-02-21 12:16:27 -0500

    added reviews get all get one and update

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -13,7 +13,17 @@ CREATE TABLE cars (
     rating NUMERIC, CHECK(rating>=0 AND rating<=5),
     featured BOOLEAN,
     image TEXT,
-    color VARCHAR(20)
+    color VARCHAR(20),
+    year VARCHAR(4) NOT NULL,
+    mileage INT NOT NULL
 );
 -- vim should be length 17 and not null
---
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    reviewer VARCHAR(50),
+    content TEXT,
+    rating NUMERIC,
+    CHECK (rating >= 0 AND rating <= 5),
+    car_id INTEGER REFERENCES cars (id)
+    ON DELETE CASCADE
+);
